094cc8f1af9a2281494f87f4f12e28e8dda1772f
Gabriel Reis <gabrieladriano.jr@gmail.com>
2026-02-21 10:24:53 -0300

    adicionei class moto e o metodo empinar

--- veiculo.py
+++ veiculo.py
@@ -2,3 +2,7 @@ class veiculo():
    def __init__(self):
       def acelerar():
          print('acelerando...')
+class moto():
+   def __init__(self):
+      def empinar_a_moto():
+         print('tenenmenenem...')
